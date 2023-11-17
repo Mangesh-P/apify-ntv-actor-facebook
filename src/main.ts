46bdeff4bf8997fb64c148d8af77118aab106b4b
MangeshP <mpimpalkar@nativo.com>
2023-11-16 17:51:11 -0800

    First Draft

---
+++ src/main.ts
@@ -0,0 +1,112 @@
+/**
+ * This template is a production ready boilerplate for developing with `PlaywrightCrawler`.
+ * Use this to bootstrap your projects using the most up-to-date code.
+ * If you're looking for examples or want to learn more, see README.
+ */
+
+// For more information, see https://docs.apify.com/sdk/js
+import { Actor, log } from 'apify';
+// For more information, see https://crawlee.dev
+import { launchPlaywright } from 'crawlee';
+// this is ESM project, and as such, it requires you to specify extensions in your relative imports
+// read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
+// note that we need to use `.js` even when inside TS files
+
+import { Page } from 'playwright';
+
+import {
+    getIframe,
+    injectFacebookEmbedIframe,
+    saveError,
+    saveOutput,
+    validateFacebookUrl,
+} from './utils.js';
+
+const DEMO_SITE = 'https://demo.ntv.io/genericsite.com/sponsoredcontent.html';
+const IFRAME_ID = 'ntv-fb-iframe';
+
+interface IInput {
+    url: string;
+}
+
+// Initialize the Apify SDK
+await Actor.init();
+
+try {
+    const input = await Actor.getInput<IInput>();
+
+    if (!input) throw new Error('Input is missing!');
+    const { url } = input;
+    // const url = 'https://www.facebook.com/MazdaUSA/photos/a.429757820362/10167170515805363';
+
+    if (!validateFacebookUrl(url)) {
+        throw new Error('Not a valid Facebook URL.');
+    }
+
+    const proxyConfiguration = await Actor.createProxyConfiguration({
+        useApifyProxy: true,
+    });
+
+    let proxyUrl: any;
+    if (proxyConfiguration) {
+        proxyUrl = await proxyConfiguration.newUrl();
+    }
+    const browser = await launchPlaywright({
+        proxyUrl,
+        launchOptions: {
+            devtools: true,
+            headless: false,
+        },
+    });
+    if (browser) {
+        log.info('Launching new page');
+        const page: Page = await browser.newPage();
+
+        await page.goto(DEMO_SITE, {
+            waitUntil: 'domcontentloaded',
+        });
+
+        // Inject facebook embed iframe
+        await injectFacebookEmbedIframe(page, {
+            iframeId: IFRAME_ID,
+            url,
+        });
+
+        // Use a random timeout to help with bot detection
+        await page.waitForTimeout(3000);
+
+        // Get content from injected facebook iframe
+        const iframeContent = await getIframe(page, IFRAME_ID);
+
+        // The iframe will still exist even if facebook blocks the current
+        // session. Thus, if there is no text or images, then it usually means it
+        // wasn't able to load the iframe content (ie. ip address blocked).
+        if (iframeContent) {
+            log.info('Extracting text and images');
+            const images: string[] = await iframeContent.$$eval(
+                'img.img',
+                (imgs: any[]) => {
+                    return imgs.map((img: any) => img.src);
+                },
+            );
+
+            const text = await iframeContent.evaluate(
+                () => document.querySelector('p')?.textContent,
+            );
+
+            if (!text && !images.length) {
+                log.error('Unable to extract text or images.');
+            } else {
+                await saveOutput(images, text);
+                log.info('Successfully extracted text and images.');
+            }
+        }
+    } else {
+        throw new Error('Browser is not defined');
+    }
+} catch (error) {
+    await saveError(error);
+} finally {
+    // Exit successfully
+    await Actor.exit();
+}
---
+++ src/utils.ts
@@ -0,0 +1,164 @@
+import { Actor, log } from 'apify';
+import { Page } from 'playwright';
+
+const { ACTOR_DEFAULT_KEY_VALUE_STORE_ID } = process.env;
+
+export const FB_ASSET_TYPE = {
+    POST: 'post',
+    VIDEO: 'video',
+};
+
+/**
+ * It injects a Facebook embed iframe into the page
+ * @param {Page} page - Page - the page object from puppeteer
+ * @param opts - {
+ * @returns The iframe element
+ */
+export async function injectFacebookEmbedIframe(
+    page: Page,
+    opts: {
+        iframeId: string;
+        url: string;
+    },
+) {
+    const { iframeId = 'ntv-fb-iframe', url = '' } = opts;
+
+    const width = '700';
+    const height = '700';
+
+    // Determine whether it's a post or a video.
+    const assetType = getAssetTypeFromUrl(url);
+    const fbEmbedBaseUrl = `https://www.facebook.com/plugins/${assetType}.php`;
+    const encodedFbUrl = encodeURIComponent(url);
+    const queryParams = `?href=${encodedFbUrl}&show_text=true&width=${width}&height=${height}&appId`;
+
+    const fullUrl = url ? fbEmbedBaseUrl + queryParams : '';
+
+    log.info('Injecting facebook embed iframe');
+    // Inject facebook embed iframe
+    return await page.evaluate(
+        ({ iframeId, fullUrl, width, height }: any) => {
+            /**
+             * <iframe
+             *  src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FMazdaUSA%2Fphotos%2Fa.429757820362%2F10167170515805363%2F%3Ftype%3D3&show_text=true&width=650"
+             *  width="700"
+             *  style="border:none;"
+             *  allowfullscreen="true"
+             *  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;"
+             * >
+             * </iframe>
+             */
+
+            const iframe = document.createElement('iframe');
+            iframe.id = iframeId;
+            iframe.src = fullUrl;
+            iframe.width = width;
+            iframe.height = height;
+            iframe.allow = 'autoplay; clipboard-write; encrypted-media; picture-in-picture;';
+            iframe.allowFullscreen = true;
+            iframe.loading = 'eager';
+
+            iframe.style.border = 'none';
+            iframe.style.overflow = 'hidden';
+
+            // log.info(`Prepending iframe to body`);
+
+            document.body.prepend(iframe);
+
+            return iframe;
+        },
+        { iframeId, fullUrl, width, height },
+    );
+}
+
+export async function getIframe(page: Page, iframeId: string) {
+    log.info(`Waiting for iframe with id: ${iframeId}`);
+    const iframeHandle = await page.waitForSelector(`#${iframeId}`, {
+        timeout: 3000,
+    });
+
+    if (iframeHandle) {
+        log.info(`Getting iframe content frame`);
+        return await iframeHandle.contentFrame();
+    }
+    return null;
+}
+
+// https://stackoverflow.com/a/37013467
+export function validateFacebookUrl(url: string) {
+    if (!url) return;
+
+    const { host } = new URL(url);
+    const fbTypes = ['photos', 'posts', 'videos', 'watch'];
+
+    // We only support specific URLs so check to see that it exists.
+    const hasValidType = fbTypes.some((type) => {
+        return url.includes(type);
+    });
+
+    if (host.includes('facebook.com') && hasValidType) {
+        log.info('It is a valid facebook URL');
+        return url;
+    }
+    return null;
+}
+
+// https://stackoverflow.com/a/37013467
+export function getAssetTypeFromUrl(url: string) {
+    if (!url) return;
+
+    const { pathname } = new URL(url);
+
+    const postTypes = ['photos', 'posts'];
+    const videoTypes = ['videos', 'watch'];
+
+    const isPost = postTypes.some((type) => {
+        return pathname.includes(type);
+    });
+
+    if (isPost) {
+        log.info('It is a post');
+        return FB_ASSET_TYPE.POST;
+    }
+
+    const isVideo = videoTypes.some((type) => {
+        return pathname.includes(type);
+    });
+
+    if (isVideo) {
+        log.info('It is a video');
+        return FB_ASSET_TYPE.VIDEO;
+    }
+
+    throw new Error('Unable to get asset type from URL.');
+}
+
+export async function saveOutput(
+    images: string[],
+    text: string | null | undefined,
+    key = 'OUTPUT',
+) {
+    log.info(`Total images Extracted - ${images.length}`);
+    images.forEach((img: string) => {
+        log.info(`Image - ${img}`);
+    });
+    if (text) {
+        log.info(`Text Extracted - ${text}`);
+    }
+
+    log.info(
+        `https://api.apify.com/v2/key-value-stores/${ACTOR_DEFAULT_KEY_VALUE_STORE_ID}/records/OUTPUT?disableRedirect=true`,
+    );
+    return await Actor.setValue(key, {
+        images,
+        text,
+    });
+}
+
+export async function saveError(errorMessage: any, key = 'ERROR') {
+    log.error(JSON.stringify(errorMessage));
+    log.info(
+        `https://api.apify.com/v2/key-value-stores/${ACTOR_DEFAULT_KEY_VALUE_STORE_ID}/records/ERROR?disableRedirect=true`,
+    );
+    return await Actor.setValue(key, errorMessage);
+}
