a57843790eda16633fa260e4ac16fa7dcf00abb2
MangeshP <mpimpalkar@nativo.com>
2023-11-20 13:19:33 -0800

    Fix quotes

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@
  */
 
 // For more information, see https://docs.apify.com/sdk/js
-import { Actor, log } from "apify";
+import { Actor, log } from 'apify';
 // For more information, see https://crawlee.dev
-import { ProxyConfiguration, launchPlaywright } from "crawlee";
+import { ProxyConfiguration, launchPlaywright } from 'crawlee';
 // this is ESM project, and as such, it requires you to specify extensions in your relative imports
 // read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
 // note that we need to use `.js` even when inside TS files
 
-import { Page } from "playwright";
+import { Page } from 'playwright';
 
 import {
     getIframe,
@@ -20,10 +20,10 @@ import {
     saveError,
     saveOutput,
     validateFacebookUrl,
-} from "./utils.js";
+} from './utils.js';
 
-const DEMO_SITE = "https://demo.ntv.io/genericsite.com/sponsoredcontent.html";
-const IFRAME_ID = "ntv-fb-iframe";
+const DEMO_SITE = 'https://demo.ntv.io/genericsite.com/sponsoredcontent.html';
+const IFRAME_ID = 'ntv-fb-iframe';
 
 interface IInput {
     url: string;
@@ -36,12 +36,12 @@ await Actor.init();
 try {
     const input = await Actor.getInput<IInput>();
 
-    if (!input) throw new Error("Input is missing!");
+    if (!input) throw new Error('Input is missing!');
     const { url, proxy } = input;
     // const url = 'https://www.facebook.com/MazdaUSA/photos/a.429757820362/10167170515805363';
 
     if (!validateFacebookUrl(url)) {
-        throw new Error("Not a valid Facebook URL.");
+        throw new Error('Not a valid Facebook URL.');
     }
 
     const proxyConfiguration = await Actor.createProxyConfiguration({
@@ -61,11 +61,11 @@ try {
         },
     });
     if (browser) {
-        log.info("Launching new page");
+        log.info('Launching new page');
         const page: Page = await browser.newPage();
 
         await page.goto(DEMO_SITE, {
-            waitUntil: "domcontentloaded",
+            waitUntil: 'domcontentloaded',
         });
 
         // Inject facebook embed iframe
@@ -84,27 +84,27 @@ try {
         // session. Thus, if there is no text or images, then it usually means it
         // wasn't able to load the iframe content (ie. ip address blocked).
         if (iframeContent) {
-            log.info("Extracting text and images");
+            log.info('Extracting text and images');
             const images: string[] = await iframeContent.$$eval(
-                "img.img",
+                'img.img',
                 (imgs: any[]) => {
                     return imgs.map((img: any) => img.src);
-                }
+                },
             );
 
             const text = await iframeContent.evaluate(
-                () => document.querySelector("p")?.textContent
+                () => document.querySelector('p')?.textContent,
             );
 
             if (!text && !images.length) {
-                log.error("Unable to extract text or images.");
+                log.error('Unable to extract text or images.');
             } else {
                 await saveOutput(images, text);
-                log.info("Successfully extracted text and images.");
+                log.info('Successfully extracted text and images.');
             }
         }
     } else {
-        throw new Error("Browser is not defined");
+        throw new Error('Browser is not defined');
     }
 } catch (error) {
     await saveError(error);
